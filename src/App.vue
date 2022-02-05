a66da912a185b65a9ba3bccdf8e9692e49070ae5
Darryl <darryl.a.nicholson@gmail.com>
2022-02-05 12:25:26 -0800

    updated vue with router

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,30 @@
 <template>
-  <h1>Reaction Time App</h1>
-  <button @click="start">Play</button>
-  <Block v-if="isPlaying" />
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
-<script>
-import Block from './components/Block.vue'
 
-export default {
-  name: 'App',
-  components: { Block },
-  data() {
-    return {
-      isPlaying: false,
-      delay: null
-    }
-  },
-  methods: {
-    start() {
-      this.delay = 2000 + Math.random() * 5000
-      this.isPlaying = true
-      console.log(this.delay)
-    }
-  }
-}
-</script>
-
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
