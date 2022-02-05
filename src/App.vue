895292f3d54f274b59fd322485a4fd1e2e1781e5
Darryl <darryl.a.nicholson@gmail.com>
2022-02-05 11:38:33 -0800

    reaction time game

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,26 @@
 <template>
-  <h1>{{title}}</h1>
-  <div v-if="showModal">
-    <Modal theme="dark" @close="toggleModal">
-      <h1>Sign Up For Newsletter</h1>
-      <p>This is where the content gose</p>
-      <template v-slot:links>
-        <a href="">Sign Up Now</a>
-        <a href="">More Info</a>
-      </template>
-    </Modal>
-  </div>
-  <button @click="toggleModal">open modal</button>
+  <h1>Reaction Time App</h1>
+  <button @click="start">Play</button>
+  <Block v-if="isPlaying" />
 </template>
 <script>
-import Modal from './components/Modal.vue'
+import Block from './components/Block.vue'
 
 export default {
   name: 'App',
-  components: { Modal },
+  components: { Block },
   data() {
     return {
-      title: 'My First Vue App',
-      header: 'Sign Up for our NewsLetter',
-      text: 'This is where the content gose',
-      showModal: false
+      isPlaying: false,
+      delay: null
     }
   },
   methods: {
-    toggleModal() {
-      this.showModal = !this.showModal;
-    },
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      console.log(this.delay)
+    }
   }
 }
 </script>
