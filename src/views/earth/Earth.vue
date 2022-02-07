a0e7725bf68049bd228f08d1b19d321d0c3f4d96
Darryl <darryl.a.nicholson@gmail.com>
2022-02-07 13:44:26 -0800

    updates
    added new json-server package
    added rest of the images

--- src/views/earth/Earth.vue
+++ src/views/earth/Earth.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="earth">
-    <h1>This is the Earth Planet page</h1>
+    <div class="wrapper">
+      <div class="planet-content">
+        <div class="planet-display">
+          <h1>This is the Earth Planet page</h1>
+        </div>
+        <div class="planet-info">
+          sidebar
+        </div>
+      </div>
+      <div class="planet-spec">
+        add spec here
+      </div>
+    </div>
   </div>
 </template>
