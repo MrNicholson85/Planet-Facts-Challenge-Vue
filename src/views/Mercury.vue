c5ba99a76db1031e9066e06df6ba92285b6557f2
Darryl <darryl.a.nicholson@gmail.com>
2022-03-09 19:53:33 -0800

    updated mercury page

--- src/views/Mercury.vue
+++ src/views/Mercury.vue
@@ -1,20 +1,34 @@
 <template>
   <div v-if="murcuryData" class="planet mercury">
     <div class="planet__image">
-      <img :src="internalImg" />
-      <!-- <img :src="gioImg" /> -->
-      <!-- <img :src="mercuryImg" /> -->
+      <img v-if="internalActive" class="planet__image-internal" :src="internalImg" />
+      <img v-if="gioActive" class="planet__image-gio" :src="gioImg" />
+      <img v-if="overviewActive || gioActive" class="planet__image-overview" :src="mercuryImg" />
     </div>
     <div class="planet__content">
-      <h1>{{ murcuryData.name }}</h1>
-      <p>{{ murcuryData.overview.content }}</p>
-      <div class="planet__source">
-        source: <a href="">Wikipedia <img :src="sourceIcon"></a>
+        <h1>{{ murcuryData.name }}</h1>
+      <div v-if="overviewActive">
+        <p>{{ murcuryData.overview.content }}</p>
+        <div class="planet__source">
+          source: <a href="">Wikipedia <img :src="sourceIcon"></a>
+        </div>
+      </div>
+      <div v-if="internalActive">
+        <p>{{ murcuryData.structure.content }}</p>
+        <div class="planet__source">
+          source: <a href="">Wikipedia <img :src="sourceIcon"></a>
+        </div>
+      </div>
+      <div v-if="gioActive">
+        <p>{{ murcuryData.geology.content }}</p>
+        <div class="planet__source">
+          source: <a href="">Wikipedia <img :src="sourceIcon"></a>
+        </div>
       </div>
       <div class="planet__specBtns">
-        <button><span>01</span>Overview</button>
-        <button><span>02</span>Internal Structure</button>
-        <button><span>03</span>Surface Geology</button>
+        <button @click="overviewToggle" :class="[overviewActive ? 'active' : '']"><span>01</span>Overview</button>
+        <button @click="internalToggle" :class="[internalActive ? 'active' : '']"><span>02</span>Internal Structure</button>
+        <button @click="gioToggle" :class="[gioActive ? 'active' : '']"><span>03</span>Surface Geology</button>
       </div>
     </div>
   </div>
@@ -34,7 +48,27 @@ export default {
       internalImg,
       gioImg,
       mercuryImg,
-      sourceIcon
+      sourceIcon,
+      overviewActive: true,
+      internalActive: false,
+      gioActive: false,
+    }
+  },
+  methods: {
+    overviewToggle() {
+      this.overviewActive = true;
+      this.internalActive = false;
+      this.gioActive = false;
+    },
+    internalToggle() {
+      this.internalActive = true;
+      this.overviewActive = false;
+      this.gioActive = false;
+    },
+    gioToggle() {
+      this.internalActive = false;
+      this.overviewActive = false;
+      this.gioActive = true;
     }
   },
   mounted() {
@@ -49,9 +83,27 @@ export default {
 <style lang="scss">
 @import '../assets/scss/variables.scss';
 
-  button {
-    &:hover {
-      background-color: $mercury;
+.planet {
+  &__image {
+    position: relative;
+
+    &-gio {
+      position: absolute;
+      width: 163px;
+      height: 199px;
+      bottom: -136px;
     }
   }
+
+  button {
+    &:hover {
+      background-color: #d8d8d82f;
+    }
+
+    &.active {
+      background-color: $mercury;
+      border-color: $mercury;
+    }
+  }
+}
 </style>
