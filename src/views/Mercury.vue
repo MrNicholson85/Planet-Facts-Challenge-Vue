848cb99e6012bc520b854a1d80088db5e7ae2c17
Darryl <darryl.a.nicholson@gmail.com>
2022-03-09 20:28:14 -0800

    completed mercury

--- src/views/Mercury.vue
+++ src/views/Mercury.vue
@@ -1,34 +1,54 @@
 <template>
   <div v-if="murcuryData" class="planet mercury">
-    <div class="planet__image">
-      <img v-if="internalActive" class="planet__image-internal" :src="internalImg" />
-      <img v-if="gioActive" class="planet__image-gio" :src="gioImg" />
-      <img v-if="overviewActive || gioActive" class="planet__image-overview" :src="mercuryImg" />
+    <div class="planet__info-container">
+      <div class="planet__image">
+        <img v-if="internalActive" class="planet__image-internal" :src="internalImg" />
+        <img v-if="gioActive" class="planet__image-gio" :src="gioImg" />
+        <img v-if="overviewActive || gioActive" class="planet__image-overview" :src="mercuryImg" />
+      </div>
+      <div class="planet__content">
+          <h1>{{ murcuryData.name }}</h1>
+        <div v-if="overviewActive">
+          <p>{{ murcuryData.overview.content }}</p>
+          <div class="planet__source">
+            source: <a href="">Wikipedia <img :src="sourceIcon"></a>
+          </div>
+        </div>
+        <div v-if="internalActive">
+          <p>{{ murcuryData.structure.content }}</p>
+          <div class="planet__source">
+            source: <a href="">Wikipedia <img :src="sourceIcon"></a>
+          </div>
+        </div>
+        <div v-if="gioActive">
+          <p>{{ murcuryData.geology.content }}</p>
+          <div class="planet__source">
+            source: <a href="">Wikipedia <img :src="sourceIcon"></a>
+          </div>
+        </div>
+        <div class="planet__specBtns">
+          <button @click="overviewToggle" :class="[overviewActive ? 'active' : '']"><span>01</span>Overview</button>
+          <button @click="internalToggle" :class="[internalActive ? 'active' : '']"><span>02</span>Internal Structure</button>
+          <button @click="gioToggle" :class="[gioActive ? 'active' : '']"><span>03</span>Surface Geology</button>
+        </div>
+      </div>
     </div>
-    <div class="planet__content">
-        <h1>{{ murcuryData.name }}</h1>
-      <div v-if="overviewActive">
-        <p>{{ murcuryData.overview.content }}</p>
-        <div class="planet__source">
-          source: <a href="">Wikipedia <img :src="sourceIcon"></a>
-        </div>
+    <div class="planet__specs">
+      <div class="planet__specs-card">
+        <div>ROTATION TIME</div>
+        <span>{{ murcuryData.rotation }}</span>
       </div>
-      <div v-if="internalActive">
-        <p>{{ murcuryData.structure.content }}</p>
-        <div class="planet__source">
-          source: <a href="">Wikipedia <img :src="sourceIcon"></a>
-        </div>
+      <div class="planet__specs-card">
+        <div>REVOLUTION TIME</div>
+        <span>{{ murcuryData.revolution }}</span>
       </div>
-      <div v-if="gioActive">
-        <p>{{ murcuryData.geology.content }}</p>
-        <div class="planet__source">
-          source: <a href="">Wikipedia <img :src="sourceIcon"></a>
-        </div>
+      <div class="planet__specs-card">
+        <div>AVERAGE TEMP.</div>
+        <span>{{ murcuryData.temperature }}</span>
       </div>
-      <div class="planet__specBtns">
-        <button @click="overviewToggle" :class="[overviewActive ? 'active' : '']"><span>01</span>Overview</button>
-        <button @click="internalToggle" :class="[internalActive ? 'active' : '']"><span>02</span>Internal Structure</button>
-        <button @click="gioToggle" :class="[gioActive ? 'active' : '']"><span>03</span>Surface Geology</button>
+      <div class="planet__specs-card">
+        <div>ROTATION TIME</div>
+        <span>{{ murcuryData.rotation }}</span>
       </div>
     </div>
   </div>
@@ -84,17 +104,6 @@ export default {
 @import '../assets/scss/variables.scss';
 
 .planet {
-  &__image {
-    position: relative;
-
-    &-gio {
-      position: absolute;
-      width: 163px;
-      height: 199px;
-      bottom: -136px;
-    }
-  }
-
   button {
     &:hover {
       background-color: #d8d8d82f;
